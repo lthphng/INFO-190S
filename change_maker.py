725f5ff1fbe02784021d29ae5e54b07b11ed90ee
lthphng <114843071+lthphng@users.noreply.github.com>
2022-10-22 15:06:33 -0400

    Update change_maker.py

--- change_maker.py
+++ change_maker.py
@@ -1,126 +1,102 @@
-nickel = 25
+nickel = 0
 dime = 25
-quarter = 25
+quarter = 0
 one = 0
 five = 0
+epsilon = 0.0196/100
 
-illegal = 0
+print('Welcome to the vending machine change maker program\nChange maker initialized.\nStock contains:')
+print(f'\t{nickel} nickels\n\t{dime} dimes\n\t{quarter} quarters\n\t{one} ones\n\t{five} fives\n')
+price = input("Enter the purchase price (xx.xx) or `q' to quit: ")
 
-print('Welcome to the vending machine change maker program\n'
-      'Change maker initialized.')
-print('Stock contains:')
-print(f'   {nickel} nickels') #5 cents
-print(f'   {dime} dimes') #10 cents
-print(f'   {quarter} quarters') #25 cents
-print(f'   {one} ones') #1 dollar = 100 cents
-print(f'   {five} fives') #5 dollars = 500 cents
+while price != 'q':
+    price = float(price)
+    while price*100%5 != 0:
+        print('Illegal price: Must be a non-negative multiple of 5 cents.\n')
+        price = input("Enter the purchase price (xx.xx) or `q' to quit: ")
+        if price == 'q':
+            break
+        else:
+            price = float(price)
+    if price != 'q':
+        print("\nMenu for deposits:\n\t'n' - deposit a nickel\n\t'd' - deposit a dime\n\t'q' - deposit a quarter\n\t'o' - deposit a one dollar bill\n\t'f' - deposit a five dollar bill\n\t'c' - cancel the purchase\n")
+        while price > 0:
+            if price > 1:
+                print(f'Payment due: {price//1:.0f} dollars and {(price - price//1)*100:.0f} cents')
+                deposit = input('Indicate your deposit: ')
+            else:
+                print(f'Payment due: {price * 100:.0f} cents')
+                deposit = str(input('Indicate your deposit: '))
+
+            if deposit != 'n' and deposit != 'd' and deposit != 'q' and deposit != 'o' and deposit != 'f' and deposit != 'c':
+                print(f'Illegal selection: {deposit}')
+            elif deposit == 'n':
+                price -= 0.05
+                nickel += 1
+            elif deposit == 'd':
+                price -= 0.1
+                dime += 1
+            elif deposit == 'q':
+                price -= 0.25
+                quarter += 1
+            elif deposit == 'o':
+                price -= 1
+                one += 1
+            elif deposit == 'f':
+                price -= 5
+                five += 1
+            else:
+                break
+        print('\nPlease take the change below:')
+        #Greedy algorithm
+        returnCur = 0
+        if price == 0:
+            print('\tNo change due.')
+        while (price <= -0.25 + epsilon or price <= -0.25 - epsilon) and quarter > 0:
+            price += 0.25
+            quarter -= 1
+            returnCur += 1
+        if returnCur > 1 or quarter >= 0:
+            if quarter < 0:
+                print(f'\t{returnCur - 1} quarters')
+            elif returnCur != 0:
+                print(f'\t{returnCur} quarters')
+        if quarter < 0:
+            quarter = 0
+        returnCur = 0
+        while (price <= -0.1 + epsilon or price <= -0.1 - epsilon) and dime > 0:
+            price += 0.1
+            dime -= 1
+            returnCur += 1
+        if returnCur > 1 or dime >= 0:
+            if dime < 0:
+                print(f'\t{returnCur - 1} dimes')
+            elif returnCur != 0:
+                print(f'\t{returnCur} dimes')
+        if dime < 0:
+            dime = 0
+        returnCur = 0
+        while (price <= -0.05 + epsilon or price <= -0.05 - epsilon) and nickel > 0:
+            price += 0.05
+            nickel -= 1
+            returnCur += 1
+        if returnCur > 1 or nickel >= 0:
+            if nickel < 0:
+                print(f'\t{returnCur - 1} nickels')
+            elif returnCur != 0:
+                print(f'\t{returnCur} nickels')
+        if nickel < 0:
+            nickel = 0
+            print(f'Machine is out of change.\nSee store manager for remaining refund.\nAmount due is: {(-price)//1:.0f} dollars and {((-price) - (-price)//1)*100:.0f} cents')
+            print(price)
+            break
+        print(f'\nStock contains:\n\t{nickel} nickels\n\t{dime} dimes\n\t{quarter} quarters\n\t{one} ones\n\t{five} fives\n')
+        price = input("Enter the purchase price (xx.xx) or `q' to quit: ")
+
+total = nickel*0.05 + dime*0.1 + quarter*0.25 + one + five*5
+print(f'\nTotal: {total//1:.0f} dollars and {(total - total//1)*100:.0f} cents')
 
 
-valid = input("\nEnter the purchase price (xx.xx) or `q' to quit: \n")
 
-while valid!='q':
-      valid=float(valid)
 
-      if valid*100%5==0:
-            print('\nMenu for deposits:\n'
-                  "  'n' - deposit a nickel\n"
-                  "  'd' - deposit a dime\n"
-                  "  'q' - deposit a quarter\n"
-                  "  'o' - deposit a one dollar bill\n"
-                  "  'f' - deposit a five dollars bill\n"
-                  "  'c' - cancel the purchase\n"
-                  )
 
-            while valid>1:
-                  print(f"Payment due: {(valid)//1:.0f} dollars and {((valid)-(valid)//1)*100:.0f} cents")
-                  select = input("Indicate your deposit: \n")
-
-                  if select != 'n' and select != 'd' and select != 'q' and select != 'o' and select != 'f' and select != 'c':
-                        illegal+=1
-                        print(f"Illegal selection: {illegal}")
-                  elif select=='c':
-                        break
-                  else:
-                        if select=='n':
-                              valid-=0.05
-                              nickel+=1
-                        elif select=='d':
-                              valid-=0.1
-                              dime+=1
-                        elif select=='q':
-                              valid-=0.25
-                              quarter+=1
-                        elif select=='o':
-                              valid-=1
-                              one+=1
-                        elif select=='f':
-                              valid-=5
-                              five+=1
-
-            while 0<valid<1:
-                  print(f"Payment due: {valid*100:.0f} cents")
-                  select=input("Indicate your deposit: ")
-                  if select != 'n' and select != 'd' and select != 'q' and select != 'o' and select != 'f' and select != 'c':
-                        illegal+=1
-                        print(f"Illegal selection: {illegal}")
-                  elif select=='c':
-                        break
-                  else:
-                        if select == 'n':
-                              valid -= 0.05
-                              nickel += 1
-                        elif select == 'd':
-                              valid -= 0.1
-                              dime += 1
-                        elif select == 'q':
-                              valid -= 0.25
-                              quarter += 1
-                        elif select == 'o':
-                              valid -= 1
-                              one += 1
-                        elif select == 'f':
-                              valid -= 5
-                              five += 1
-
-            if valid<0 or select=='c':
-                  print("\nPlease take the change below.")
-                  if quarter>0 and (-valid>=0.25 or valid>0.25):
-                        if valid<0:
-                              print(f"\t{-valid//0.25:.0f} quarters")
-                              quarter -= -valid // 0.25
-                        else:
-                              print(f"\t{valid//0.25:.0f} quarters")
-                              quarter -= valid // 0.25
-                        valid%=25
-                  if dime>0 and (-valid>=0.1 or valid>0.1):
-                        if valid<0:
-                              print(f"\t{-valid//0.10:.0f} dimes")
-                              dime -= -valid // 0.1
-                        else:
-                              print(f"\t{valid // 0.10:.0f} dimes")
-                              dime -= valid // 0.1
-                        valid%=10
-
-                  if nickel>0 and (-valid>0 or valid>0):
-                        if valid<0:
-                              print(f"\t{-valid//0.05:.0f} nickels")
-                              nickel -= -valid // 0.05
-                        else:
-                              print(f"\t{valid//0.05:.0f} nickels")
-                              nickel -= valid // 0.05
-
-            print('\nStock contains:\n'
-                  f'  {nickel:.0f} nickels\n'  # 5 cents
-                  f'  {dime:.0f} dimes\n'  # 10 cents
-                  f'  {quarter:.0f} quarters\n'  # 25 cents
-                  f'  {one:.0f} ones\n'  # 1 dollar = 100 cents
-                  f'  {five:.0f} fives'  # 5 dollars = 500 cents
-                  )
-
-      else:
-            print('\nIllegal price: Must be a non-negative multiple of 5 cents.')
-      valid = input("\nEnter the purchase price (xx.xx) or 'q' to quit: ")
-
-if valid=='q':
-      total = nickel*0.05 + dime*0.1 + quarter*0.25 + one*1 + five*5
-      print(f"\nTotal: {(total)//1:.0f} dollars and {((total)-(total)//1)*100:.0f} cents")
