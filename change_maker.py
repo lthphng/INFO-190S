bd05ce70ef6479c172e1e55e96c39d1653c27eec
lthphng <114843071+lthphng@users.noreply.github.com>
2022-10-22 15:32:13 -0400

    Update change_maker.py

--- change_maker.py
+++ change_maker.py
@@ -1,35 +1,35 @@
-nickel = 0
+nickel = 25
 dime = 25
-quarter = 0
+quarter = 25
 one = 0
 five = 0
 epsilon = 0.0196/100
 
 print('Welcome to the vending machine change maker program\nChange maker initialized.\nStock contains:')
-print(f'  {nickel} nickels\n  {dime} dimes\n  {quarter} quarters\n  {one} ones\n  {five} fives\n')
+print(f'   {nickel} nickels\n   {dime} dimes\n   {quarter} quarters\n   {one} ones\n   {five} fives\n')
 price = input("Enter the purchase price (xx.xx) or `q' to quit: ")
 
 while price != 'q':
     price = float(price)
     while price*100%5 != 0:
-        print('\nIllegal price: Must be a non-negative multiple of 5 cents.\n')
-        price = input("Enter the purchase price (xx.xx) or `q' to quit: \n")
+        print('Illegal price: Must be a non-negative multiple of 5 cents.\n')
+        price = input("Enter the purchase price (xx.xx) or `q' to quit: ")
         if price == 'q':
             break
         else:
             price = float(price)
     if price != 'q':
-        print("\nMenu for deposits:\n  'n' - deposit a nickel\n  'd' - deposit a dime\n  'q' - deposit a quarter\n  'o' - deposit a one dollar bill\n  'f' - deposit a five dollar bill\n  'c' - cancel the purchase\n")
+        print("\nMenu for deposits:\n   'n' - deposit a nickel\n   'd' - deposit a dime\n   'q' - deposit a quarter\n   'o' - deposit a one dollar bill\n   'f' - deposit a five dollar bill\n   'c' - cancel the purchase\n")
         while price > 0:
             if price > 1:
-                print(f'\nPayment due: {price//1:.0f} dollars and {(price - price//1)*100:.0f} cents')
-                deposit = input('Indicate your deposit: \n')
+                print(f'Payment due: {price//1:.0f} dollars and {(price - price//1)*100:.0f} cents')
+                deposit = input('Indicate your deposit: ')
             else:
-                print(f'\nPayment due: {price * 100:.0f} cents')
-                deposit = str(input('Indicate your deposit: \n'))
+                print(f'Payment due: {price * 100:.0f} cents')
+                deposit = str(input('Indicate your deposit: '))
 
             if deposit != 'n' and deposit != 'd' and deposit != 'q' and deposit != 'o' and deposit != 'f' and deposit != 'c':
-                print(f'\nIllegal selection: {deposit}')
+                print(f'Illegal selection: {deposit}')
             elif deposit == 'n':
                 price -= 0.05
                 nickel += 1
@@ -51,16 +51,16 @@ while price != 'q':
         #Greedy algorithm
         returnCur = 0
         if price == 0:
-            print('  No change due.')
+            print('   No change due.')
         while (price <= -0.25 + epsilon or price <= -0.25 - epsilon) and quarter > 0:
             price += 0.25
             quarter -= 1
             returnCur += 1
         if returnCur > 1 or quarter >= 0:
             if quarter < 0:
-                print(f'  {returnCur - 1} quarters')
+                print(f'   {returnCur - 1} quarters')
             elif returnCur != 0:
-                print(f'  {returnCur} quarters')
+                print(f'   {returnCur} quarters')
         if quarter < 0:
             quarter = 0
         returnCur = 0
@@ -70,9 +70,9 @@ while price != 'q':
             returnCur += 1
         if returnCur > 1 or dime >= 0:
             if dime < 0:
-                print(f'  {returnCur - 1} dimes')
+                print(f'   {returnCur - 1} dimes')
             elif returnCur != 0:
-                print(f'  {returnCur} dimes')
+                print(f'   {returnCur} dimes')
         if dime < 0:
             dime = 0
         returnCur = 0
@@ -82,21 +82,16 @@ while price != 'q':
             returnCur += 1
         if returnCur > 1 or nickel >= 0:
             if nickel < 0:
-                print(f'  {returnCur - 1} nickels')
+                print(f'   {returnCur - 1} nickels')
             elif returnCur != 0:
-                print(f'  {returnCur} nickels')
+                print(f'   {returnCur} nickels')
         if nickel < 0:
             nickel = 0
             print(f'Machine is out of change.\nSee store manager for remaining refund.\nAmount due is: {(-price)//1:.0f} dollars and {((-price) - (-price)//1)*100:.0f} cents')
             print(price)
             break
-        print(f'\nStock contains:\n  {nickel} nickels\n  {dime} dimes\n  {quarter} quarters\n  {one} ones\n  {five} fives\n')
+        print(f'\nStock contains:\n   {nickel} nickels\n   {dime} dimes\n   {quarter} quarters\n   {one} ones\n   {five} fives\n')
         price = input("Enter the purchase price (xx.xx) or `q' to quit: ")
 
 total = nickel*0.05 + dime*0.1 + quarter*0.25 + one + five*5
 print(f'\nTotal: {total//1:.0f} dollars and {(total - total//1)*100:.0f} cents')
-
-
-
-
-
