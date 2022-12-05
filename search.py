b080ff0082ce8fb6a60e99aab26c7cbfb1672c3d
lthphng <114843071+lthphng@users.noreply.github.com>
2022-12-05 17:16:53 -0500

    Create search.py

---
+++ search.py
@@ -0,0 +1,22 @@
+import string
+import urllib.request
+import re
+
+def read_article_file(url):
+    req = urllib.request.urlopen(url)
+    text = req.read()
+    text = text.decode('UTF-8')
+    return text
+
+def text_to_article_list(text):
+    article_list = re.split('<NEW ARTICLE>', text)
+    article_list.pop(0)
+    return article_list
+
+def split_words(text):
+    each = text.splitlines()
+    each.pop(0)
+    return each
+
+def scrub_word(text):
+    return text.strip(string.punctuation)
